9a4bdfc874794ef4ba639dca2fea71a02eade84e
Aniket Patil <aniketp1597@gmail.com>
2023-04-08 22:57:45 -0400

    getting all feature matches

--- Utils/FeatureUtils.py
+++ Utils/FeatureUtils.py
@@ -6,8 +6,8 @@ from IPython import embed
 class FeatureUtils:
     def __init__(self):
         self.n_features = []
-        self.matching_files = ['matching2.txt']
-        self.match_file_idxes = [2]
+        self.matching_files = ['matching1.txt', 'matching2.txt', 'matching3.txt', 'matching4.txt']
+        self.match_file_idxes = [1, 2, 3, 4]
         self.matches = {}
         self.total_matches = 0
         # self.inliers = {}
